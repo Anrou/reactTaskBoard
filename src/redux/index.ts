12d1196ec34569e732163c2b6279e3067c1f122f
MaxKomar <maxkomar65@gmail.com>
2017-12-05 16:16:01 +0200

    gh-pages

--- src/redux/index.ts
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from "redux";
-import listsReducer from "./list"
-import cardsReducer from "./card"
-
-export default combineReducers({
-    lists: listsReducer,
-    cards:cardsReducer
-});
